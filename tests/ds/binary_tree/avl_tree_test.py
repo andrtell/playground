f546aba1b6664d5e4db3318e71aedb07b094430e
andrtell <116459057+andrtell@users.noreply.github.com.>
2024-09-30 20:43:19 +0200

    add even more tests

--- tests/ds/binary_tree/avl_tree_test.py
+++ tests/ds/binary_tree/avl_tree_test.py
@@ -13,3 +13,17 @@ def test_sorted(count):
     src = list(set(src))
     src.sort()
     assert list(t) == src
+
+@given(st.integers(1, 100))
+def test_min(count):
+    src = util.random_list(count, 0, 10000)
+    t = AVLTree()
+    t.insert_from(src)
+    assert t.min() == min(src)
+
+@given(st.integers(1, 100))
+def test_max(count):
+    src = util.random_list(count, 0, 10000)
+    t = AVLTree()
+    t.insert_from(src)
+    assert t.max() == max(src)
